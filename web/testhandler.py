c7fe6d1663de4b9db8d93ce3882d931ab94899ce
pierregueth <pierregueth@64e6cce1-733f-0410-9f1c-cec4c888497b>
2008-01-29 01:32:16 +0000

    slmfdjdjgfskldjg
    git-svn-id: http://svn.berlios.de/svnroot/repos/kolmognus/trunk@25 64e6cce1-733f-0410-9f1c-cec4c888497b

--- web/testhandler.py
+++ web/testhandler.py
@@ -1,6 +1,11 @@
 from mod_python import apache
+from web import sql
 
 def handler(req):
     req.content_type='text/plain'
-    req.write('hello how are you??')
+    req.write('hello how are you??\n')
+    db=sql.connect_db()
+    logins=sql.login_list(db)
+    req.write(' '.join(logins)+'\n')
+    req.write('bye!!\n')
     return apache.OK
