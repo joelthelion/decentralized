eec06bddd51f2def0a52544f7be254bbc50f6e57
pierregueth <pierregueth@64e6cce1-733f-0410-9f1c-cec4c888497b>
2008-01-29 16:05:45 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/kolmognus/trunk@28 64e6cce1-733f-0410-9f1c-cec4c888497b

--- web/delicious.py
+++ web/delicious.py
@@ -1,51 +1,102 @@
 #!/usr/bin/env python
-
 #delicious content parser for sax xml parser implementation
+
+
 import xml.sax.handler
-class DeliciousHandler(xml.sax.handler.ContentHandler):
+import xml.sax
+
+def get_recent_urls():
+	"""Returns a list of URLs concerning a certain tag"""
+	feed='http://delicious.com/rss/'
+	handler=DeliciousTagHandler()
+	xml.sax.parse(feed,handler)
+	return handler.urls
+
+def get_urls_for_tag(tag):
+	"""Returns a list of URLs concerning a certain tag"""
+	feed='http://delicious.com/rss/tag/%s' % tag
+	handler=DeliciousTagHandler()
+	xml.sax.parse(feed,handler)
+	return handler.urls
+
+def get_delicious_data_for_url(url):
+	"""Returns all the relevent delicious data concerning URL"""
+	import md5
+	feed='http://delicious.com/rss/url/%s' % md5.md5(url).hexdigest()
+	handler=DeliciousURLHandler()
+	xml.sax.parse(feed,handler)
+	tags=list(set(handler.tags))
+	import time
+	pub_date = min([time.mktime(time.strptime(delicious_date_string,"%Y-%m-%jT%H:%M:%SZ")) for delicious_date_string in handler.bookmark_dates])
+	return (handler.authors,tags,handler.descriptions,pub_date)
+	
+class DeliciousURLHandler(xml.sax.handler.ContentHandler):
+	"""Parses a delicious rss of the form delicious/rss/md5"""
+	def __init__(self):
+		self.logins=[]
+		self.tags=[]
+		self.descriptions=[]
+		self.authors=[]
+		self.bookmark_dates=[]
+		self.in_item=False
+		self.in_description=False
+		self.in_author=False
+		self.in_tags=False
+		self.in_bookmark_date=False
+	def startElement(self,name,attributes):
+		if name == "item":
+			self.in_item=True
+		elif name =="description":
+			self.in_description=True
+		elif name =="dc:subject":
+			self.in_tags=True
+		elif name =="dc:creator":
+			self.in_author=True
+		elif name =="dc:date":
+			self.in_bookmark_date=True
+	def characters(self, data):
+		if self.in_item and data != "":
+			if self.in_description: 
+				self.descriptions.append(data)
+			elif self.in_tags:
+				self.tags.extend(data.split())
+			elif self.in_author:
+				self.authors.append(data)
+			elif self.in_bookmark_date:
+				self.bookmark_dates.append(data)
+	def endElement(self,name):
+		if name == "item":
+			self.in_item=False
+		elif name =="description":
+			self.in_description=False
+		elif name =="dc:subject":
+			self.in_tags=False
+		elif name =="dc:creator":
+			self.in_author=False
+		elif name =="dc:date":
+			self.in_bookmark_date=False
+
+class DeliciousTagHandler(xml.sax.handler.ContentHandler):
+	"""Parses a delicious rss of the form delicious/rss/tag/c++"""
 	def __init__(self):
 		self.in_item=False
-		self.in_title=False
-		self.in_tag=False
-		self.tags={}
-	def __repr__(self):
-		return '\n'.join([url+': '+title+'\n\t'+' '.join(tags) for url,(title,tags) in handler.tags.items()]).encode('utf-8')
+		self.urls=[]
 
 	def startElement(self,name,attributes):
 		if name=="item":
-			self.current_item=attributes["rdf:about"]
-			self.current_title=''
-			self.current_tags=''
-			self.in_item=True
-		elif name=="title":
-			self.in_title=True
-		elif name=="dc:subject":
-			self.in_tag=True
+			self.urls.append(attributes["rdf:about"])
 	def characters(self, data):
-		if not self.in_item:
-			return
-		if self.in_title:
-			self.current_title+=data
-		elif self.in_tag:
-			self.current_tags+=data
+		pass
 	def endElement(self,name):
-		if name=="dc:subject":
-			self.in_tag=False
-		elif name=="title":
-			self.in_title=False
-		elif name=="item":
-			self.in_item=False
-			self.tags[self.current_item]=(self.current_title,self.current_tags.split(' '))
+		pass
 
 if __name__=="__main__":
-	import sys
-	if len(sys.argv)>1:
-		feed=sys.argv[1]
-	else:
-		feed="http://del.icio.us/rss/tag/c++"
+	
+	print get_urls_for_tag("c++")
+	print get_recent_urls()
 
-	import xml.sax
-	handler=DeliciousHandler()
-	xml.sax.parse(feed,handler)
-	print handler
-	print len(handler.tags)
+	logins,tags,descriptions,date=get_delicious_data_for_url("http://www.lemonde.fr/")
+	print descriptions
+	print logins
+	print tags
+	print date
---
+++ web/fetcher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#EXTERNAL API
+def add_tag_request(tag):
+	pass
+
+#INTERNAL FUNCTIONS
+
+if __name__ == '__main__':
+	pass
