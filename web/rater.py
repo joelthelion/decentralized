317a714163710b7455a063e57488f7d574d6dcf8
joelschaerer <joelschaerer@64e6cce1-733f-0410-9f1c-cec4c888497b>
2008-03-08 18:52:27 +0000

    first performance improvements to the rater, plus better interface
    git-svn-id: http://svn.berlios.de/svnroot/repos/kolmognus/trunk@78 64e6cce1-733f-0410-9f1c-cec4c888497b

--- web/rater.py
+++ web/rater.py
@@ -5,7 +5,7 @@ import classifier
 
 if __name__ == '__main__':
     rerate_delay="0 00:00:01"
-    stories=sql.request("select id,url_md5,url,symbols from story where (not isnull(fetch_date)) and (isnull(rated_date) or addtime(rated_date,%s) < now())" , rerate_delay)
+    stories=sql.request("select id,url_md5,url,symbols from story where addtime(fetch_date,'12:00:00') > now() and (not isnull(fetch_date)) and (isnull(rated_date) or addtime(rated_date,%s) < now())" , rerate_delay) #do not rerate old news
     users=sql.request("select id,login from kolmognus_user")
     classifiers={}
     for user_id,login in users: #rate stories for each user
