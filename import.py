81cc5556e61aac07b8962ae01fc6bb12fe9d56b4
Joel Schaerer <joel.schaerer@laposte.net>
2008-04-29 10:26:13 +0200

    bugfix

--- import.py
+++ import.py
@@ -1,8 +1,10 @@
 import cPickle
 import os
-a=cPickle.load(open(os.path.expanduser("~/.popurls_alreadyseen.pck")))
+f=open(os.path.expanduser("~/.popurls_alreadyseen.pck"))
+a=cPickle.load(f)
+f.close()
 for k in a:
     if len(k)<2:
         now,count=a[k]
         a[k]=now,0
-cPickle.dump(a,open("/home/joel/.popurls_alreadyseen.pck","wb"),-1)
+cPickle.dump(a,open(os.path.expanduser("~/.popurls_alreadyseen.pck"),"wb"),-1)
