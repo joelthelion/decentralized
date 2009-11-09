608a272e237ae82ab23b7a26a5b8e66b00f0dd0b
Joël Schaerer <joel.schaerer@insa-lyon.fr>
2009-11-09 14:22:06 +0100

    unicode, + workaround the duplicate entry problem

--- new/rsslinks.py
+++ new/rsslinks.py
@@ -11,8 +11,7 @@ class Link(Base):
     """A link is a potentially interesting URL, together with all the relevant metadata
     to help evaluate it"""
     __tablename__ = "links"
-    id = Column(Integer,primary_key=True)
-    url = Column(Unicode)
+    url = Column(Unicode,primary_key=True)
     title = Column(Unicode)
     source = Column(Unicode)
     date = Column(DateTime)
@@ -29,14 +28,15 @@ def get_links(rssfeed):
     from datetime import datetime
     f=parse(rssfeed)
     s=Session()
-    for item in f.entries:
-        s.add(Link(item.link,item.title,f.url,datetime.fromtimestamp(time.mktime(item.date_parsed))))
+    for i in f.entries:
+        s.merge(Link(unicode(i.link),unicode(i.title),unicode(f.url)\
+            ,datetime.fromtimestamp(time.mktime(i.date_parsed))))
     s.commit()
 
 if __name__ == '__main__':
     from sqlalchemy import create_engine
     engine=create_engine("sqlite:///test.db")
     Session.configure(bind=engine)
-    #Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
     get_links("http://www.lemonde.fr/rss/une.xml")
