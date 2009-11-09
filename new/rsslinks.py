11b378fb0da0c87000e655f7224373b97642cad8
Joel Schaerer <joelthelion@laposte.net>
2009-11-09 22:12:22 +0100

    more complete database schema

--- new/rsslinks.py
+++ new/rsslinks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import database as db
-from sqlalchemy import Table, Column, Integer, String, Unicode,DateTime
+from sqlalchemy import Table, Column, Integer, Unicode,DateTime,ForeignKey
+from sqlalchemy.orm import relation,backref
 
 class Link(db.Base):
     """A link is a potentially interesting URL, together with all the relevant metadata
@@ -9,14 +10,30 @@ class Link(db.Base):
     __tablename__ = "links"
     url = Column(Unicode,primary_key=True)
     title = Column(Unicode)
-    source = Column(Unicode)
+    sources = relation("LinkSource",backref=backref('link'))
     date = Column(DateTime)
 
-    def __init__(self,u,t,s,d):
+    def __init__(self,u,t,d):
         self.url=u
         self.title=t
-        self.source=s
         self.date=d
+    def __repr__(self):
+        return self.title.encode('utf-8')+\
+            " ("+self.url.encode('utf-8')+")"
+
+class LinkSource(db.Base):
+    """The sources of a link. Can contain additional pieces of info
+    only relevant to a certain source, such as points for a reddit link"""
+    __tablename__ = "linksources"
+    link_url = Column(Unicode,ForeignKey('links.url'),primary_key=True)
+    """source is a source identifier, such as the URL of an RSS feed, or
+    the jabber id of a friend"""
+    source = Column(Unicode,primary_key=True)
+    def __init__(self,l,s):
+        self.link_url=l
+        self.source=s
+    def __repr__(self):
+        return "link \"" + self.link_url.encode('utf-8')+"\" linked to by "+self.source.encode('utf-8')
 
 def get_links(rssfeed):
     from feedparser import parse
@@ -25,10 +42,16 @@ def get_links(rssfeed):
     f=parse(rssfeed)
     s=db.Session()
     for i in f.entries:
-        s.merge(Link(unicode(i.link),unicode(i.title),unicode(f.url)\
-            ,datetime.fromtimestamp(time.mktime(i.date_parsed))))
+        newlink=Link(unicode(i.link),unicode(i.title),datetime.fromtimestamp(time.mktime(i.date_parsed)))
+        newlink.sources=[LinkSource(newlink.url,unicode(f.url))]
+        s.merge(newlink)
     s.commit()
 
 if __name__ == '__main__':
     db.Base.metadata.create_all(db.engine)
     get_links("http://www.lemonde.fr/rss/une.xml")
+    #import time
+    #t=time.time()
+    #for i in range(10000):
+    #    get_links("file://une.xml")
+    #print time.time()-t
