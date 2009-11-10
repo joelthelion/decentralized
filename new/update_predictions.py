3ccd5ef38e31f5ab97c633383825be0eb4a89f0e
Joël Schaerer <joel.schaerer@insa-lyon.fr>
2009-11-10 16:44:05 +0100

    standard suffix for classifiers

--- new/update_predictions.py
+++ new/update_predictions.py
@@ -4,11 +4,11 @@
 def get_classifiers():
     import re
     import os
-    return [module[:-3] for module in os.listdir('classifiers') if re.match('^[^_].*\.py$',module)]
+    return [module[:-3] for module in os.listdir('classifiers') if re.search('_class\.py$',module)]
 
 
 if __name__ == '__main__':
     import classifiers
     for cl_name in get_classifiers():
         current=__import__('classifiers.'+cl_name,fromlist=[classifiers])
-        print current.predict(None)
+        print cl_name,":",current.predict(None)
