a79cf6b5339dd0ca733799bc00bb700d634a7407
Joel Schaerer <joelthelion@laposte.net>
2009-11-13 18:50:14 +0100

    for some reason tokenizing is a bad idea

--- new/classifiers/titlewords_class.py
+++ new/classifiers/titlewords_class.py
@@ -5,11 +5,11 @@ filter=Bayesian('titlewords.pck',2)
 
 #See http://en.wikipedia.org/wiki/Naive_Bayes_classifier
 def predict(link):
-    words=tokenize(link.title)
+    words=link.title.split()
     return filter.predict(words)
 
 def train(links):
-    filter.train([(tokenize(l.title),l.evaluation) for l in links])
+    filter.train([(l.title.split(),l.evaluation) for l in links])
 
 def print_self():
     print "A naive Bayesian classifier on title words. Selection of words:"
