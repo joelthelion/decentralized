643966c1f289c76c796de71cbd68dc5d58164d99
Joel Schaerer <joelthelion@laposte.net>
2009-11-15 23:19:21 +0100

    this should be a bit more robust

--- new/rsslinks.py
+++ new/rsslinks.py
@@ -8,24 +8,28 @@ def get_links(rssfeed):
     from datetime import datetime
     from time import time
     from random import random
-    t=time()
-    f=parse(rssfeed)
-    session=db.Session()
-    added=0
-    for i in f.entries:
-        current_link=session.query(Link).filter_by(url=unicode(i.link)).first()
-        if not current_link:
-            added+=1
-            #can't trust the link date, it is dependent on RSS time fuse
-            current_link=Link(unicode(i.link),unicode(i.title),\
-                datetime.fromtimestamp(time()+random()*30))
-            session.add(current_link)
-        elif unicode(f.url) in (s.source for s in current_link.sources):
-            continue # entry is already there
-        current_link.sources.append(LinkSource(current_link.url,unicode(f.url)))
-    session.commit()
-    print rssfeed,"(%.2fs)"%(time()-t)
-    return added
+    try:
+        t=time()
+        f=parse(rssfeed)
+        session=db.Session()
+        added=0
+        for i in f.entries:
+            current_link=session.query(Link).filter_by(url=unicode(i.link)).first()
+            if not current_link:
+                added+=1
+                #can't trust the link date, it is dependent on RSS time fuse
+                current_link=Link(unicode(i.link),unicode(i.title),\
+                    datetime.fromtimestamp(time()+random()*30))
+                session.add(current_link)
+            elif unicode(f.url) in (s.source for s in current_link.sources):
+                continue # entry is already there
+            current_link.sources.append(LinkSource(current_link.url,unicode(f.url)))
+        session.commit()
+        print rssfeed,"(%.2fs)"%(time()-t)
+        return added
+    except Exception as e:
+        print "Exception when parsing %s" % rssfeed
+        print e
 
 if __name__ == '__main__':
     from utils import feeds
