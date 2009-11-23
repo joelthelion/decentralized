7e88937b732a04649b47ebf7ba1750f61e4e047b
Joel Schaerer <joelthelion@laposte.net>
2009-11-24 00:05:37 +0100

    some experiments on clustering

---
+++ new/cluster.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import utils
+from datamodel import *
+import database as db
+from utils import tokenize
+
+def basic_similarity(l1,l2):
+    w1,w2=tokenize(l1),tokenize(l2)
+    return len(set(w1).intersection(w2))/float(len(w1+w2))
+
+def better_similarity(l1,l2):
+    from math import log
+    w1,w2=tokenize(l1),tokenize(l2)
+    return sum(len(w) for w in (set(w1).intersection(w2)))/log(len(w1+w2))
+
+def freq_sim(l1,l2,freqz):
+    from math import log
+    w1,w2=tokenize(l1),tokenize(l2)
+    return sum(1./log(freqz[w]) for w in (set(w1).intersection(w2)))/log(len(w1+w2))
+    
+
+def argmax(array,fn):
+    return array[(max((fn(i),n) for n,i in enumerate(array)))[1]]
+
+def frequencies(links):
+    freqz={}
+    for l in links:
+        for w in tokenize(l.title):
+            freqz[w]=freqz.get(w,0)+1
+    return freqz
+
+if __name__ == '__main__':
+    import random
+    s=db.Session()
+    links=s.query(Link).all()
+    initial=random.choice(links)
+    freqz=frequencies(links)
+    for func in [basic_similarity,better_similarity,lambda l1,l2:freq_sim(l1,l2,freqz)]:
+        links=s.query(Link).all()
+        selected=initial
+        print "===================  ",func,"  ======================"
+        for i in range(6):
+            links.remove(selected)
+            print selected.title.encode('utf-8')
+            temp= argmax(links,lambda l:func(selected.title,l.title))
+            #links.append(selected)
+            selected=temp
+
+
+    #for d in utils.most_frequent_duos(fwords):
+    #    print d
