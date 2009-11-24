88861ad1a7800f27a7db6ca6d84905eb340f2a30
Joël Schaerer <joel.schaerer@insa-lyon.fr>
2009-11-24 13:40:00 +0100

    iii

--- new/word_similarities.py
+++ new/word_similarities.py
@@ -7,6 +7,7 @@ def compute_sims(link,ws):
         for w2 in words:
             if w != w2:
                 pair=frozenset((w,w2))
+                assert(len(list(pair))>1)
                 ws[pair]=ws.get(pair,0)+1
     return word_similarities
         
@@ -15,7 +16,7 @@ def compute_freq_sims(link,ws,freqz):
     words=utils.tokenize(link.title)
     for w in words:
         for w2 in words:
-            if w is not w2:
+            if w != w2:
                 pair=frozenset((w,w2))
                 ws[pair]=ws.get(pair,0)+1./(freqz[w]+freqz[w2]+15)
     return word_similarities
@@ -29,6 +30,7 @@ if __name__ == '__main__':
     freqz=utils.word_frequencies(links)
     for l in links:
         word_similarities=compute_freq_sims(l,word_similarities,freqz)
-    a=[(list(s),f) for s,f in word_similarities.items()]
+    a=[(tuple(s),f) for s,f in word_similarities.items()]
     a.sort(key=lambda i:i[1])
-    print a[-100:]
+    for (w1,w2),f in a[-100:]:
+        print w1,w2,"%.2f"%f
