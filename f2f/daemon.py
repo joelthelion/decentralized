e0f35853dd2abdb77d54862a7e8dfd69c530825e
Joël Schaerer <joel.schaerer@laposte.net>
2010-07-18 11:56:54 +0200

    persistence needs to be refactored

--- f2f/daemon.py
+++ f2f/daemon.py
@@ -5,17 +5,27 @@ import persistence
 
 post_queue=Queue()
 my_posts=[]
+print >>stderr,"hello!!!"
 
-persistence_dict={"my_posts" : my_posts}
-persistence_doer=persistence.do_persistence(persistence_dict)
 
 def add_post():
+    print >>stderr, "New post taken into account."
     new_post=post_queue.get()
     my_posts.append(new_post)
 
 def start_daemon():
+    persistence_dict=persistence.restore()
+    if persistence_dict:
+        my_posts.extend(persistence_dict["my_posts"])
+	persistence_dict["my_posts"] = my_posts
+    else:
+        print >>stderr, "No persistence file found, starting with empty containers"
+        persistence_dict={"my_posts" : my_posts}
+    persistence_doer=persistence.persistence(persistence_dict)
+
     import time
     while True:
+	print >>stderr, my_posts
         if not post_queue.empty():
 	    add_post()
             #print >>stderr, post_queue.get()
--- f2f/persistence.py
+++ f2f/persistence.py
@@ -1,18 +1,25 @@
 import cPickle as pickle
 import os
+from sys import stderr
 
 storage_file="~/.config/f2f.pck"
 
-def do_persistence(persistence_dict):
+def persistence(persistence_dict):
     import time
     t=0
     while True:
-        if time.time() - t > 120: #save every two minutes
-	    with open(os.path.expanduser(storage_file),"w") as file:
+        if time.time() - t > 10: #save every two minutes
+	    with open(os.path.expanduser(storage_file),"w") as myfile:
                 t=time.time()
-                pickle.dump(persistence_dict,file)
+                pickle.dump(persistence_dict,myfile)
+		print >>stderr, "Wrote %d links to the disk" % len(persistence_dict["my_posts"])
                 yield None
+        else: yield None
 
-def restore_dict()
-    with open(os.path.expanduser(storage_file),"w") as file:
-        return pickle.load(file)
+def restore():
+    try:
+        with open(os.path.expanduser(storage_file),"r") as myfile:
+            return pickle.load(myfile)
+    except IOError as e:
+	print >>stderr, e,type(e)
+        return {}
