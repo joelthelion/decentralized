7dea6855af46547532e7277d1842551a90581dd2
Joël Schaerer <joel.schaerer@laposte.net>
2010-08-12 16:14:29 +0200

    simple friend server

--- f2f/friendserver.py
+++ f2f/friendserver.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
+# a simple webserver that plays the role of OneSwarm's "community server"
 
 from wsgiref.simple_server import make_server
+from cgi import parse_qs, escape
+import json
+from random import sample
 from persistence import PersistentObject,basedir
 
 class FriendStorageObject(PersistentObject):
@@ -8,33 +12,38 @@ class FriendStorageObject(PersistentObject):
      def __init__(self):
         PersistentObject.__init__(self)
         if not self.__dict__.has_key("friends"):
-            self.friends=[]
+            self.friends={}
 
 storage=FriendStorageObject()
 
 def not_found(environ, start_response):
    response_body="Error 404: Not found"
    status = '404 Not Found'
-   print environ
    start_response(status, [])
    return [response_body]
 
-get_friends_html= """
-<html>
-<title>Here are some friends!</title>
-<body>
-%s
-</body>
-</html>"""
-
 def get_friends(environ,start_response):
-    pass
+    response_body=json.dumps(sample(storage.friends.items(),min(20,len(storage.friends.items()))))
+    response_headers = [('Content-Type', 'text/json;charset=utf-8'),
+                  ('Content-Length', str(len(response_body)))]
+    start_response('200 OK', response_headers)
+    return [response_body]
 
 def post(environ,start_response):
     d = parse_qs(environ['QUERY_STRING'])
     jid = escape(d.get('jid', [''])[0])
     ip = environ['REMOTE_ADDR']
-    storage.friends.append((jid,ip))
+    if not ip in storage.friends.keys():
+        storage.friends[ip]=jid
+        response_body="<html><title>ok</title><body>ok!</body></html>"
+    else:
+        storage.friends[ip]=jid
+        response_body="<html><title>ok</title><body>you're already there! jid updated.</body></html>"
+    storage.store()
+    response_headers = [('Content-Type', 'text/html;charset=utf-8'),
+                  ('Content-Length', str(len(response_body)))]
+    start_response('200 OK', response_headers)
+    return [response_body]
 
 import re
 
